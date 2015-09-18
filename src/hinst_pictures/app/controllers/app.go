7e20cb79105314a3763f1c46e28da242a0efbc8b
hinst <hinstance@yandex.ru>
2015-09-18 18:11:51 +0300

    Implemented dataBase transaction management using method interceptors

--- src/hinst_pictures/app/controllers/app.go
+++ src/hinst_pictures/app/controllers/app.go
@@ -7,6 +7,38 @@ type App struct {
 	Transaction *TTransaction
 }
 
-func (c *App) Index() revel.Result {
-	return c.Render()
+func (this *App) Index() revel.Result {
+	return this.Render()
+}
+
+func (this *App) prepareDB() revel.Result {
+	this.Transaction = OpenTransaction()
+	if this.Transaction != nil {
+		return nil
+	} else {
+		revel.ERROR.Println("Could not obtain database transaction")
+		return this.RenderTemplate("dbError.html")
+	}
+}
+
+func (this *App) prepare() revel.Result {
+	var result = this.prepareDB()
+	if result != nil {
+		revel.ERROR.Println("Database error")
+		return result
+	}
+	return result
+}
+
+func (this *App) finalize() revel.Result {
+	if this.Transaction != nil {
+		this.Transaction.Close()
+		this.Transaction = nil
+	}
+	return nil
+}
+
+func init() {
+	revel.InterceptMethod((*App).prepare, revel.BEFORE)
+	revel.InterceptMethod((*App).finalize, revel.AFTER)
 }
