16964cfe08587f0ec97cd41d4cac7ae8fd6e6ef5
hinst <hinstance@yandex.ru>
2015-09-21 14:00:54 +0300

    db script updated

--- data/createUsersTable.sql
+++ data/createUsersTable.sql
@@ -1,2 +1,2 @@
-CREATE TABLE 'Users' ('Name' char primary key, 'Password' char, 'Admin' integer, 'CreationDate' date);
-CREATE TABLE 'Sessions' ('Name' char, 'SessionKey' char, 'LastRequest' date, primary key ('Name', 'SessionKey');
+CREATE TABLE "Users" ("Name" char primary key, "Password" char, "Admin" integer, "CreationDate" date);
+CREATE TABLE "Sessions" ("Name" char, "SessionKey" char, "LastRequest" date, primary key ("Name", "SessionKey"));
